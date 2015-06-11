b1edf8c4ed1c7c4c06583c65953d89d48b05c088
Ramesh Kumar <rameshmimit@users.noreply.github.com>
2015-06-11 15:19:35 +0530

    Changing to download_url to dummy url.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 class awsutils::params {
   $ec2_api_tools  = 'ec2-api-tools.zip'
   $ec2_ami_tools  = 'ec2-ami-tools.zip'
-  $download_url   = 'http://puppetmaster01.iaistg.quadanalytix.com'
+  $download_url   = 'http://local_yum_repo.com'
   $dest_path      = '/opt/aws'
   $aws_access_key = 'undef'
   $aws_secret_key = 'undef'
